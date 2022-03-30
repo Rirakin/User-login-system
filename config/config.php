f3649252dbc519d0a266fd59a01e45c605e743be
Paweł <pawel.balkait@gmail.com>
2022-03-30 23:55:15 +0200

    code cleanup and reformat

--- config/config.php
+++ config/config.php
@@ -1,13 +1,8 @@
-<?php 
+<?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'login_system');
+const DB_SERVER = 'localhost';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = '';
+const DB_NAME = 'login_system';
 
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-if($mysqli === false)
-{
-    die("ERROR: Could not connect. " . $mysqli->connect_error);
-}
--- login.php
+++ login.php
@@ -1,8 +1,7 @@
-<?php 
+<?php
 session_start();
 
-if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true)
-{
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     header('location: welcome.php');
     exit;
 }
@@ -12,75 +11,56 @@ require_once "config/config.php";
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
 
-if($_SERVER['REQUEST_METHOD'] == "POST")
-{
-    if(empty(trim($_POST['username'])))
-    {
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (empty(trim($_POST['username']))) {
         $username_err = "Please enter username.";
-    }
-    else 
-    {
+    } else {
         $username = trim($_POST["username"]);
     }
 
-    if(empty(trim($_POST['password'])))
-    {
+    if (empty(trim($_POST['password']))) {
         $password_err = "Please enter your password.";
-    }
-    else 
-    {
+    } else {
         $password = trim($_POST['password']);
     }
 
-    if(empty($username_err) && empty($password_err))
-    {
+    if (empty($username_err) && empty($password_err)) {
         $sql = "SELECT id, username, password FROM users WHERE username = ?";
-      
-        if($stmt = $mysqli->prepare($sql))
-        {
+
+        if ($stmt = $mysqli->prepare($sql)) {
             $stmt->bind_param("s", $param_username);
-            
+
             $param_username = $username;
-            
-            if($stmt->execute())
-            {
+
+            if ($stmt->execute()) {
                 $stmt->store_result();
-                
-                if($stmt->num_rows == 1)
-                {                    
+
+                if ($stmt->num_rows == 1) {
                     $stmt->bind_result($id, $username, $hashed_password);
-                    if($stmt->fetch())
-                    {
-                        if(password_verify($password, $hashed_password))
-                        {
+                    if ($stmt->fetch()) {
+                        if (password_verify($password, $hashed_password)) {
                             session_start();
-                            
+
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;                            
-                            
+                            $_SESSION["username"] = $username;
+
                             header("location: welcome.php");
-                        } 
-                        else
-                        {
+                        } else {
                             $login_err = "Invalid username or password.";
                         }
                     }
-                } 
-                else
-                {
+                } else {
                     $login_err = "Invalid username or password.";
                 }
-            } 
-            else
-            {
+            } else {
                 echo "Something went wrong. Please try again later.";
             }
 
             $stmt->close();
         }
     }
-    
+
     $mysqli->close();
 
 }
@@ -88,47 +68,51 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
     <title>Login</title>
-  </head>
-  <body>
-    <div class="container mt-5" style="max-width: 30rem;">
-        <h1>Sign in</h1>
-        <p>Create an account by filling out the form</p>
-        <?php 
-        if(!empty($login_err))
-        {
-          echo '<div class="alert alert-danger">' . $login_err . '</div>';
-        }
-        ?>
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
-            <div class="form-group">
-              <label for="username">Username</label>
-              <input type="text" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" name="username" id="username" aria-describedby="helpId" value="<?php echo $username; ?>">
-              <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>
-            <div class="form-group mt-2">
-              <label for="password">Password</label>
-              <input type="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" name="password" id="password" aria-describedby="helpId" value="<?php echo $password; ?>">
-              <span class="invalid-feedback"><?php echo $password_err; ?></span>
+</head>
+<body>
+<div class="container mt-5" style="max-width: 30rem;">
+    <h1>Sign in</h1>
+    <p>Create an account by filling out the form</p>
+    <?php
+    if (!empty($login_err)) {
+        echo '<div class="alert alert-danger">' . $login_err . '</div>';
+    }
+    ?>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
+                   name="username" id="username" aria-describedby="helpId" value="<?php echo $username; ?>">
+            <span class="invalid-feedback"><?php echo $username_err; ?></span>
+        </div>
+        <div class="form-group mt-2">
+            <label for="password">Password</label>
+            <input type="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"
+                   name="password" id="password" aria-describedby="helpId" value="<?php echo $password; ?>">
+            <span class="invalid-feedback"><?php echo $password_err; ?></span>
 
-            </div>
-            <button type="submit" class="btn btn-primary mt-3">Login</button>
-            <p>Don't have an account? <a href="register.php">Sign up</a></p>
-        </form>
+        </div>
+        <button type="submit" class="btn btn-primary mt-3">Login</button>
+        <p>Don't have an account? <a href="register.php">Sign up</a></p>
+    </form>
 
-    </div>
-    
-  
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</div>
 
- 
-  </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
+        crossorigin="anonymous"></script>
+
+
+</body>
 </html>
